3da6724c3c80f21a89a47cb8dc3f31947ff22040
tempati <dadsbads97@gmail.com>
2021-04-11 19:35:10 +0900

    改善点を直した

--- SlideshowApp2/SizeUpViewController.swift
+++ SlideshowApp2/SizeUpViewController.swift
@@ -8,30 +8,29 @@
 import UIKit
 
 class SizeUpViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     var images:UIImage!
-//    var images =  UIImage(named: "neko1")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.imageView.image = images
         
-    
+        
     }
-        // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- SlideshowApp2/ViewController.swift
+++ SlideshowApp2/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBAction func tapimage(_ sender: UITapGestureRecognizer) {
     }
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var proceedButton: UIButton!
+    
     
     var timer:Timer!
     
@@ -22,7 +25,8 @@ class ViewController: UIViewController {
     var imageArray: [UIImage] = [
         UIImage(named: "neko1")!,
         UIImage(named: "neko2")!,
-        UIImage(named: "neko3")!
+        UIImage(named: "neko3")!,
+        UIImage(named: "inu1")!,
     ]
     
     override func viewDidLoad() {
@@ -30,53 +34,66 @@ class ViewController: UIViewController {
         self.imageView.image = imageArray[nowIndex]
         // Do any additional setup after loading the view.
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        self.timer.invalidate()
         let sizeUpViewController:SizeUpViewController = segue.destination as! SizeUpViewController
         sizeUpViewController.images = self.imageView.image!
     }
     
     @IBAction func back(_ sender: Any) {
+        if self.timer != nil {
+            self.backButton.isEnabled = false
+        }
         if self.timer == nil {
+            self.proceedButton.isEnabled = true
+            
             if self.nowIndex == 0 {
                 self.nowIndex = imageArray.count-1
-//            self.nowIndex = 2
-        }else if self.nowIndex != 0 {
-            self.nowIndex -= 1
-        }
-        self.imageView.image = imageArray[nowIndex]
+            }else if self.nowIndex != 0 {
+                self.nowIndex -= 1
+            }
+            self.imageView.image = imageArray[nowIndex]
         }
     }
     
     @IBAction func start(_ sender: Any) {
         if self.timer == nil {
+            self.proceedButton.isEnabled = false
+            self.backButton.isEnabled = false
             timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
             self.startButton.setTitle("停止", for: .normal)
         }else {
+            self.proceedButton.isEnabled = true
+            self.backButton.isEnabled = true
             self.timer.invalidate()
             self.timer = nil
-            self.startButton.setTitle("再生", for: .normal)        }
-        
+            self.startButton.setTitle("再生", for: .normal)
+        }
     }
     
     @IBAction func proceed(_ sender: Any) {
-        if self.timer == nil {
-            if self.nowIndex != 2 {
-            self.nowIndex += 1            
-            }else if self.nowIndex == 2 {
-            self.nowIndex = 0
+        if self.timer != nil {
+            self.proceedButton.isEnabled = false
         }
-        self.imageView.image = imageArray[nowIndex]
+        if self.timer == nil {
+            self.proceedButton.isEnabled = true
+            
+            if self.nowIndex != imageArray.count-1 {
+                self.nowIndex += 1            
+            }else if self.nowIndex == imageArray.count-1 {
+                self.nowIndex = 0
+            }
+            self.imageView.image = imageArray[nowIndex]
         }
     }
     @objc func changeImage(){
-        self.imageView.image = imageArray[nowIndex]
-
         self.nowIndex += 1
         
         if self.nowIndex == imageArray.count {
             self.nowIndex = 0
         }
+        self.imageView.image = imageArray[nowIndex]
         
     }
     
